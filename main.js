8a1a98aa6cb7d586aa1fd30bbe42fa22346f7fba
maycon345 <146994229+maycon345@users.noreply.github.com>
2025-06-09 08:20:56 -0300

    .

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ document . querySelector('.parametro-senhas_botao');
 const numerosenha = ducument.querySelector('.parametro-senha_texto');
 numerosenha.textContent=5;
 
-const botoes = document . querySelector('parametro-senha_botao');
+const botoes = document . querySelectorAll('parametro-senha_botao');
 
 console .log (botoes);
