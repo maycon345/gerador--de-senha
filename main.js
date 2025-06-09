0cd083d187adc41a000acb5c278dbfa2d4ccd8ef
maycon345 <146994229+maycon345@users.noreply.github.com>
2025-06-09 08:26:38 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 document . querySelector('.parametro-senhas_botao');
 const numerosenha = ducument.querySelector('.parametro-senha_texto');
-numerosenha.textContent=5;
+
+let tamanhoSenha = 12;
+
+numerosenha.textContent=tamanhoSenha;
 
 const botoes = document . querySelectorAll('parametro-senha_botao');
+botoes[0] .onclick = diminuirtamanho;
+
 
 console .log (botoes);
+function diminuirTamanho(){
+
+}
